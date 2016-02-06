f9300cc2fdf0dd894f981c67b458eda60a809d82
Dario Chamoro <dario.chamorro.v@gmail.com>
2016-02-06 15:54:19 -0500

    FloatingActionButton agregado

--- app/src/main/java/com/example/estacionvl_tc_014/cinestar/DetalleActivity.java
+++ app/src/main/java/com/example/estacionvl_tc_014/cinestar/DetalleActivity.java
@@ -1,18 +1,22 @@
 package com.example.estacionvl_tc_014.cinestar;
 
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.graphics.Palette;
 import android.support.v7.widget.Toolbar;
 import android.widget.ImageView;
 
 import com.example.estacionvl_tc_014.cinestar.models.Pelicula;
 import com.example.estacionvl_tc_014.cinestar.util.C;
+import com.squareup.picasso.Callback;
 import com.squareup.picasso.Picasso;
 
-public class DetalleActivity extends AppCompatActivity {
+
+public class DetalleActivity extends AppCompatActivity implements Callback {
 
     ImageView imageView;
     Toolbar toolbar;
@@ -32,14 +36,32 @@ public class DetalleActivity extends AppCompatActivity {
 
         setSupportActionBar(toolbar);
 
-        int colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
-        collapsingToolbarLayout.setContentScrimColor(colorPrimary);
+
 
         Pelicula p = C.data.get(pos);
 
         collapsingToolbarLayout.setTitle(p.getNombre());
-        Picasso.with(this).load(Uri.parse(p.getUrl())).into(imageView);
+        Picasso.with(this).load(Uri.parse(p.getUrl())).into(imageView, this);
 
+
+
+
+    }
+
+    @Override
+    public void onSuccess() {
+        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
+
+        Palette palette = Palette.from(drawable.getBitmap()).generate();
+
+        int colorPrimary = ContextCompat.getColor(this, R.color.colorPrimary);
+        int colorVibrant = palette.getVibrantColor(colorPrimary);
+
+        collapsingToolbarLayout.setContentScrimColor(colorVibrant);
+    }
+
+    @Override
+    public void onError() {
 
     }
 }
